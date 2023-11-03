87d2c40c5bad8325d2d24882ad4369d9a916c846
Mar Alonso <mar.alonso@udc.es>
2023-11-03 16:31:48 +0100

    ReactiveSearch configurado

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,56 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import {ReactiveBase, DataSearch, ReactiveList, ResultList} from '@appbaseio/reactivesearch';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const { ResultListWrapper } = ReactiveList;
+
+const App = () => {
+  return (
+    <ReactiveBase
+      app="shein_prod" // Nombre de tu aplicación en Elasticsearch
+      url="http://localhost:9200" // URL de tu clúster de Elasticsearch
+    >
+      <div>
+        <h1>Datos de Elasticsearch</h1>
+        <DataSearch
+          componentId="search"
+          dataField="nombre" // Campo en el que se realizará la búsqueda
+          placeholder="Buscar productos"
+          react={{ and: ['SearchResult'] }}
+        />
+
+        <ReactiveList
+
+    componentId="SearchResult"
+    dataField = "nombre"
+    react={{ and: ['search'] }}
+>
+    {({ data}) => (
+        <ResultListWrapper>
+            {
+                data.map(item => (
+                    <ResultList key={item._id}>
+                        <ResultList.Content>
+                            <ResultList.Title
+                                dangerouslySetInnerHTML={{
+                                    __html: item.nombre
+                                }}
+                            />
+                        </ResultList.Content>
+                    </ResultList>
+                ))
+            }
+        </ResultListWrapper>
+    )}
+</ReactiveList>
+
+
+      </div>
+    </ReactiveBase>
+  );
+};
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
