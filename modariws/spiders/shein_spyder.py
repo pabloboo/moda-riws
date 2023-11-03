428b123f27d8d2c1365a702f1820a2583329e6d8
Mar Alonso <mar.alonso@udc.es>
2023-11-03 22:30:24 +0100

    modificaciones en los índices de los spyders

--- modariws/spiders/shein_spyder.py
+++ modariws/spiders/shein_spyder.py
@@ -1,78 +1,78 @@
-import scrapy
-import json
-from bs4 import BeautifulSoup
-from modariws.items import ProductoShein
-from elasticsearch import Elasticsearch
-from scrapy import Request
-from scrapy.linkextractors import LinkExtractor
-
-
-class SheinSpider(scrapy.Spider):
-    name = "shein"
-    allowed_domains = ['shein.com']
-    start_urls = ['https://es.shein.com']
-    es = Elasticsearch([{'host': 'localhost', 'port': 9200, 'scheme': 'http'}])
-
-    def parse(self, response):
-        producto = ProductoShein()
-
-        # Use Beautiful Soup to parse the response content
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        links = LinkExtractor(
-            allow_domains=['shein.com'],
-        ).extract_links(response)
-
-        # Extract product links using Beautiful Soup
-        outlinks = []  # List to store all links
-        for link in links:
-            url = link.url
-            outlinks.append(url)  # Añadimos el enlace en la lista
-            yield Request(url, callback=self.parse)
-
-        # self.logger.info(response.text)
-
-        # Check if the page contains product information
-        product_details = soup.find('div', class_='product-intro__description')
-        if product_details:
-            # Extract product URL and description
-            producto['url'] = response.url
-
-            # Extract the product name
-            product_name = soup.find('h1', class_='product-intro__head-name')
-            if product_name:
-                name = product_name.get_text()
-                print(f'Nombre: {name}')
-                producto['nombre'] = name
-
-            # Extract the price
-            price_div = soup.find('div', class_='product-intro__head-mainprice')
-            if price_div:
-                price = price_div.find('span').get_text()
-                print(f'Precio: {price}')
-                producto['precio'] = price
-
-            color_element = response.css('div.color-block')
-
-            # Extraer el texto del elemento
-            color = color_element.css('span.color-999::text').get()
-            print(f'Color: {color}')
-            producto['color'] = color
-
-            producto['links'] = outlinks
-
-            def custom_serialize(producto):
-                # Create a dictionary with product data
-                serialized_producto = {
-                    'url': producto['url'],
-                    'nombre': producto['nombre'],
-                    'precio': producto['precio'],
-                    'color': producto['color']
-                }
-
-                # Convert the dictionary to a JSON string
-                return json.dumps(serialized_producto)
-
-            self.es.index(index='shein_prod', body=custom_serialize(producto))
-
-            yield producto
+# import scrapy
+# import json
+# from bs4 import BeautifulSoup
+# from modariws.items import ProductoShein
+# from elasticsearch import Elasticsearch
+# from scrapy import Request
+# from scrapy.linkextractors import LinkExtractor
+#
+#
+# class SheinSpider(scrapy.Spider):
+#     name = "shein"
+#     allowed_domains = ['shein.com']
+#     start_urls = ['https://es.shein.com']
+#     es = Elasticsearch([{'host': 'localhost', 'port': 9200, 'scheme': 'http'}])
+#
+#     def parse(self, response):
+#         producto = ProductoShein()
+#
+#         # Use Beautiful Soup to parse the response content
+#         soup = BeautifulSoup(response.text, 'html.parser')
+#
+#         links = LinkExtractor(
+#             allow_domains=['shein.com'],
+#         ).extract_links(response)
+#
+#         # Extract product links using Beautiful Soup
+#         outlinks = []  # List to store all links
+#         for link in links:
+#             url = link.url
+#             outlinks.append(url)  # Añadimos el enlace en la lista
+#             yield Request(url, callback=self.parse)
+#
+#         # self.logger.info(response.text)
+#
+#         # Check if the page contains product information
+#         product_details = soup.find('div', class_='product-intro__description')
+#         if product_details:
+#             # Extract product URL and description
+#             producto['url'] = response.url
+#
+#             # Extract the product name
+#             product_name = soup.find('h1', class_='product-intro__head-name')
+#             if product_name:
+#                 name = product_name.get_text()
+#                 print(f'Nombre: {name}')
+#                 producto['nombre'] = name
+#
+#             # Extract the price
+#             price_div = soup.find('div', class_='product-intro__head-mainprice')
+#             if price_div:
+#                 price = price_div.find('span').get_text()
+#                 print(f'Precio: {price}')
+#                 producto['precio'] = price
+#
+#             color_element = response.css('div.color-block')
+#
+#             # Extraer el texto del elemento
+#             color = color_element.css('span.color-999::text').get()
+#             print(f'Color: {color}')
+#             producto['color'] = color
+#
+#             producto['links'] = outlinks
+#
+#             def custom_serialize(producto):
+#                 # Create a dictionary with product data
+#                 serialized_producto = {
+#                     'url': producto['url'],
+#                     'nombre': producto['nombre'],
+#                     'precio': producto['precio'],
+#                     'color': producto['color']
+#                 }
+#
+#                 # Convert the dictionary to a JSON string
+#                 return json.dumps(serialized_producto)
+#
+#             self.es.index(index='shein_prod', body=custom_serialize(producto))
+#
+#             yield producto
