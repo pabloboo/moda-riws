a22aa91e694536d764361a93fa8ad813a4a39f8c
pabloboo <pabloboocastrofeito@gmail.com>
2023-11-16 02:17:10 +0100

    Pruebas Docker en windows y cambios en configuración Docker

--- execute-scrapy.sh
+++ execute-scrapy.sh
@@ -3,6 +3,24 @@
 # Give permissions to write the file
 chmod +w productos.json
 
+# Configure Elasticsearch for fields
+curl -X PUT "http://172.24.0.2:9200/productos/_mapping" -H "Content-Type: application/json" -d '{
+  "properties": {
+    "color": {
+      "type": "text",
+      "fielddata": true
+    },
+    "tallas": {
+      "type": "text",
+      "fielddata": true
+    },
+    "marca": {
+      "type": "text",
+      "fielddata": true
+    }
+  }
+}'
+
 # Execute Scrapy crawl
 scrapy crawl hym -s CLOSESPIDER_ITEMCOUNT=10
 scrapy crawl nicoli -s CLOSESPIDER_ITEMCOUNT=10
